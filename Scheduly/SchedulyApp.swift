3c632c4a71e1318f4d8272f066afc6a18d706805
Inan Mahmud <inan.mahmud@cefalo.com>
2023-11-25 22:01:24 +0600

    Initial Commit

---
+++ Scheduly/SchedulyApp.swift
@@ -0,0 +1,17 @@
+//
+//  SchedulyApp.swift
+//  Scheduly
+//
+//  Created by cefalo on 25/11/23.
+//
+
+import SwiftUI
+
+@main
+struct SchedulyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
