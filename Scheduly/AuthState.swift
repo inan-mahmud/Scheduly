ac42051ebc709edc135d46ec8acb97cc9d70c7c1
Inan Mahmud <inan.mahmud@cefalo.com>
2023-11-28 12:57:35 +0600

    add auth state

---
+++ Scheduly/AuthState.swift
@@ -0,0 +1,20 @@
+//
+//  AuthState.swift
+//  Scheduly
+//
+//  Created by cefalo on 27/11/23.
+//
+
+import Foundation
+import GoogleSignIn
+import GoogleSignInSwift
+
+enum AuthState {
+    case loggedIn(user: GIDGoogleUser)
+    case loggedOut
+}
+
+enum AuthError : Error {
+    case wrongCredentials
+    case unknown
+}
